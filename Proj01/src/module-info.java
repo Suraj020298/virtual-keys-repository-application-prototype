c0afd20eec75e439ab9e897de2c6a9e93abbd70f
Suraj020298 <surajashoksomkuwar.com>
2023-04-17 20:52:07 +0530

    my first commit

---
+++ Proj01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author suraj
+ *
+ */
+module Proj01 {
+}
---
+++ Proj01/src/mypackage/Welcome.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class Welcome {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Soon i'm become a good java full stack developer");
+	}
+
+}
